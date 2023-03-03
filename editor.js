0525fcab49aad1b0933b2eea297c322fe3532e99
MarcBrejner <marc@kaabrejner.dk>
2023-03-03 12:53:06 +0100

    two editors and working parse button

--- editor.js
+++ editor.js
@@ -1,13 +1,19 @@
-var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('editor1'), {
-    lineNumbers: true
+var codeMirrorEditor = CodeMirror.fromTextArea(document.getElementById('editorWindow'), {
+    lineNumbers: true,
+    value: ""
 })
 
-myCodeMirror.on('change',function(cMirror){
+var codeMirrorPretty = CodeMirror.fromTextArea(document.getElementById('prettyWindow'), {
+    lineNumbers: true,
+    value: "",
+    editable: false
+})
+
+codeMirrorEditor.on('change',function(cMirror){
     // get value right from instance
+    codeMirrorPretty.setValue(cMirror.getValue());
     console.log(cMirror.getValue());
 });
-
-const a = (async () => { const data = await initialize(); return data })()
 
 const Parser = window.TreeSitter;
 async function initialize_parser() {
@@ -18,33 +24,13 @@ async function initialize_parser() {
   return parser;
 }
 
-var parseer = await initialize();
-parseer.parse("$x:=1+1;")
-function clickclick(parser){
-    var tree = parser.parse(cMirror.getValue());
-    console.log(tree.rootNode.toString());  
+async function print_parse(source_code){
+    var p = await initialize_parser();
+    var t = p.parse(source_code)
+    console.log(t.rootNode.toString())
 }
 
 
-// 1. Call helloCatAsync passing a callback function,
-//    which will be called receiving the result from the async operation
-console.log("1. function called...")
-helloCatAsync(function(result) {
-    // 5. Received the result from the async function,
-    //    now do whatever you want with it:
-    var p = result;
-});
-
-// 2. The "callback" parameter is a reference to the function which
-//    was passed as an argument from the helloCatAsync call
-function helloCatAsync(callback) {
-    console.log("2. callback here is the function passed as argument above...")
-    // 3. Start async operation:
-    setTimeout(function() {
-    console.log("3. start async operation...")
-    console.log("4. finished async operation, calling the callback, passing the result...")
-        // 4. Finished async operation,
-        //    call the callback passing the result as argument
-        callback('Nya');
-    }, Math.random() * 2000);
+function clickclick(parser){
+    print_parse(myCodeMirror.getValue());
 }
