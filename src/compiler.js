86c887ab9f6c7e3131fd109550da520e41f9d628
MarcBrejner <marc@kaabrejner.dk>
2023-03-27 13:58:21 +0200

    return type bytecode from compile

--- src/compiler.js
+++ src/compiler.js
@@ -39,7 +39,7 @@ function compile_writer(statement){
 
 
 
-function compile_assign(statement, l_pc, assign_type){
+function compile_assign(statement){
     var writer = compile_writer(statement);
     var assign_type = statement.child(1).text;
     var expression = statement.child(2);
@@ -49,26 +49,26 @@ function compile_assign(statement, l_pc, assign_type){
         case 1: // reader
             var op = assign_type === ":=" ? OP.ASSIGN : OP.COND;
             var reader = compile_reader(expression.child(0));
-            return [op, l_pc, [writer, reader]];
+            return new bytecode(op, [writer, reader])
         case 2: // oper, reader
             var op = assign_type === ":=" ? OP.ASSIGN_UN : OP.COND_UN;
             var reader = compile_reader(expression.child(1));
             var opr = expression.child(0).text;
-            return [op, l_pc, [writer, opr, reader]];
+            return new bytecode(op, [writer, opr, reader])
         case 3: // reader, oper, reader
             var op = assign_type === ":=" ? OP.ASSIGN_BIN : OP.COND_BIN;
             var reader1 = compile_reader(expression.child(0));
             var opr = expression.child(1).text;
             var reader2 = compile_reader(expression.child(2));
-            return [op, l_pc, [writer, reader1, opr, reader2]];
+            return new bytecode(op, [writer, reader1, opr, reader2])
     }
 }
 
 function compile_statement(statement, l_pc){
     if(statement.childCount == 1 && statement.text == 'syscall'){
-        return [OP.SYSCALL,l_pc];
+        return new bytecode(OP.SYSCALL)
     }else{
-        return compile_assign(statement, l_pc, "assign")
+        return compile_assign(statement)
     }
 }
 
@@ -107,13 +107,13 @@ function compile_statements(statements){
     let l_pc = 0;
     for(let s_i = 0; s_i < statements.childCount; s_i++){
         let statement = statements.child(s_i);
-        let line_number = parseInt(statement.startPosition.row);
+        //let line_number = parseInt(statement.startPosition.row);
         switch(statement.type){
             case 'label':
                 labels[statement.text] = l_pc;
                 break;
             case 'statement':
-                var bytecode = compile_statement(statement, line_number)
+                var bytecode = compile_statement(statement)
                 instructions.push(bytecode);
                 l_pc++;
                 break;
