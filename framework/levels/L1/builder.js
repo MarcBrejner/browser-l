25cba1ae0ed6543d6d1049a232ff1e26e49ce036
gernsoe <christian.gernsoe@gmail.com>
2023-05-25 16:58:38 +0200

    Forgot one file

--- framework/levels/L1/builder.js
+++ framework/levels/L1/builder.js
@@ -1,14 +1,14 @@
 class L1Builder extends L0Builder {
-
     handle(node) {
         if (node.type === "goto") {
-            this.goto(node.child(1));
+            this.goto(node)
         } else {
             return super.handle(node);
         }
     }
 
-    goto(reader) {
+    goto(node) {
+        var reader = node.child(1);
         var _reader1;
         if (reader.type === "label") {
             _reader1 = new Reader(RT.LABEL, reader.text);
@@ -18,5 +18,6 @@ class L1Builder extends L0Builder {
         var _reader2 = new Reader(RT.NUMBER, 1);
         var _writer = new Writer(WT.REGISTER, '$!');
         this.statements.push(new ByteCode(OP.ASSIGN_BIN, [true, _writer, _reader1, '-', _reader2]));
+        this.set_ECS(node)
     }
 }
